ea9ced533080be1b270d2e8c651a29d3d6be19a8
Karl Jeong <aeuveritas@gmail.com>
2021-02-27 09:32:17 +0900

    Refactor directories

--- lib/screens/note/view/note_page.dart
+++ lib/screens/note/view/note_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tonote/screens/widgets/widgets.dart';
 
 import 'package:webscrollbar/webscrollbar.dart';
 
--- lib/screens/note/widgets/widgets.dart
+++ lib/screens/note/widgets/widgets.dart
@@ -1,3 +1,1 @@
-export 'horizontal_split_view.dart';
-export 'vertical_split_view.dart';
 export 'date_button.dart';
